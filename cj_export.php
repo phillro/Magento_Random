e3d8272030e4203e966bec4db9a960d7eb2fbcf2
Phillip Rosen <phill.rosen@gmail.com>
2012-04-04 14:29:26 -0400

    execute cj and license

---
+++ cj_export.php
@@ -0,0 +1,15 @@
+<?php
+//require_once $argv[1].'app/Mage.php';
+require_once './app/Mage.php';
+
+umask(0);
+Mage::App()->loadArea('admin');
+Mage::getSingleton('core/session', array('name'=>'adminhtml'));
+$cj = new Phillro_Commissionjunction_Helper_Data();
+$buffer= $cj->generateProductCSVHeader();
+$buffer.= $cj->generateProductCSVBody();
+file_put_contents('media/cj_export.csv',$buffer);
+
+$adapter=Mage::getSingleton('imagecdn/adapter_amazons3');
+$adapter->save('/cj_export.csv','media/cj_export.csv');
+?>
